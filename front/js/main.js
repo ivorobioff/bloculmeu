8a115cdcabc1bbab3557507983820a1c2799eab5
Igor Vorobioff <i_am_vib@yahoo.com>
2013-01-24 10:10:30 +0200

    Update front/js/main.js
    some1

--- front/js/main.js
+++ front/js/main.js
@@ -1,16 +1,14 @@
 var Views = {};
 
-$(function(){
-	Views.AbstractForm = Class.extend({
-		
-		_url: '',
-		_id: 'signup-form',
-		_el: null,
-		
-		initialize: function(){
-			this._el = $('#' + this._id);
-			this._url = this._el.attr('action');
-			alert(this._url);
-		}
-	});
+Views.AbstractForm = Class.extend({
+	
+	_url: '',
+	_id: 'signup-form',
+	_el: null,
+	
+	initialize: function(){
+		this._el = $('#' + this._id);
+		this._url = this._el.attr('action');
+		alert(this._url);
+	}
 });
