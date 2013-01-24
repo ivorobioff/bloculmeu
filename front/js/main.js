17a1bb9541ae695fd7692e4f01030d2c6348a391
Igor Vorobioff <i_am_vib@yahoo.com>
2013-01-24 10:38:49 +0200

    Update front/js/main.js
    interface abstractform

--- front/js/main.js
+++ front/js/main.js
@@ -5,10 +5,28 @@ Views.AbstractForm = Class.extend({
 	_url: '',
 	_id: 'signup-form',
 	_el: null,
+	_data: {},
 	
 	initialize: function(){
 		this._el = $('#' + this._id);
 		this._url = this._el.attr('action');
-		alert(this._url);
-	}
+		this._el.submit($.proxy(function(){
+			this._data = this._el.serialize();
+			this.beforeSubmit();
+			$.post(this._url, this._data, $.proxy(function(res){
+				this.afterSubmit(res);
+				if (typeof res.status == 'success'){
+					this.success(res.data);
+				} else {
+					this.error(res.data);
+				}
+			}, this));
+			return false;
+		}, this));
+	},
+	
+	beforeSubmit: function(){},
+	afterSubmit: function(data){},
+	success: function(data){},
+	error: function(data){}
 });
