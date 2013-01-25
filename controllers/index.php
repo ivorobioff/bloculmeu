4bb916dfb02527451dbca0862d82ca9ad913e229
Igor Vorobioff <i_am_vib@yahoo.com>
2013-01-25 11:29:10 +0200

    Update controllers/index.php
    auth

--- controllers/index.php
+++ controllers/index.php
@@ -3,11 +3,6 @@ class Controllers_Index extends Controllers_Common
 {
 	public function index()
 	{
-		if (!is_auth())
-		{
-			redirect_signin();
-		}
-
 		$this->_view->render('main/index.phtml');
 	}
 }
