34e56f3b0a9bb992785720b94f5d91f2ac1e3d6c
ivorobioff <i_am_vib@yahoo.com>
2013-01-30 09:10:45 +0200

    other neighbores

--- controllers/neighbors.php
+++ controllers/neighbors.php
@@ -5,6 +5,7 @@ class Controllers_Neighbors extends Controllers_Common
 	{
 		$model = new Models_Neighbors();
 		$this->_view->assign('neighbors_list', $model->get4Main());
+		$this->_view->assign('other_neighbors_list', $model->getOther());
 		$this->_view->render('neighbors/index.phtml');
 	}
 }
--- models/neighbors.php
+++ models/neighbors.php
@@ -17,4 +17,24 @@ class Models_Neighbors
 
 		return $res;
 	}
+
+	public function getOther()
+	{
+		$users_buildings_table = new Db_UsersBuildings();
+		$users_buildings_table->setAlias('ub');
+
+		$courtyard_table = new Db_Courtyards();
+		$courtyard_table->setAlias('c');
+
+		$profiles_table = new Db_Profiles();
+		$profiles_table->setAlias('p');
+
+		return $courtyard_table
+			->select('p.*')
+			->join($users_buildings_table, 'ub.building_id=c.building_id')
+			->join($profiles_table, 'p.user_id=ub.user_id')
+			->where('c.user_id', Db_Currents::getUserInfo('id'))
+			->groupBy('ub.user_id')
+			->fetchAll();
+	}
 }
