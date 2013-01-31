b9019057adc65eff7de550ea2d71adc560af610f
Igor Vorobioff <i_am_vib@yahoo.com>
2013-01-31 16:54:38 +0200

    Update models/dialogs.php
    dialog

--- models/dialogs.php
+++ models/dialogs.php
@@ -5,11 +5,13 @@ class Models_Dialogs
 	{
 		$table = new Db_Messages();
 		$table
-			->where('(sender_id='.Db_Currents::getUserInfo('id').') OR (receiver_id='.Db_Currents::getUserInfo('id').')');
+			->where('(sender_id='.Db_Currents::getUserInfo('id').' OR receiver_id='.Db_Currents::getUserInfo('id').')');
 		
 		if ($active_user_id)
 		{
-			$table->where('sender_id != '.$active_user_id.' OR receiver_id != '.$active_user_id);
+			$table->where('sender_id != '.$active_user_id.' AND receiver_id != '.$active_user_id);
 		}
+		
+		
 	}
 }
