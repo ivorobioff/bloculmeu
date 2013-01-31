944136ddb93da074b8cd547d2da8db3e1bba1405
Igor Vorobioff <i_am_vib@yahoo.com>
2013-01-31 17:02:25 +0200

    Update models/dialogs.php
    asdasd

--- models/dialogs.php
+++ models/dialogs.php
@@ -6,6 +6,8 @@ class Models_Dialogs
 		$table = new Db_Messages();
 		$table
 			->select('IF(sender_id="'.Db_Currents::getUserInfo('id').'", receiver_id, sender_id) AS another_user')
+			->select('IF(sender_id="'.Db_Currents::getUserInfo('id').'", sender_id, receiver_id) AS me')
+			->select('id')
 			->where('(sender_id="'.Db_Currents::getUserInfo('id').'" OR receiver_id="'.Db_Currents::getUserInfo('id').'")');
 		
 		if ($active_user_id)
@@ -15,6 +17,9 @@ class Models_Dialogs
 				->where('receiver_id !=', $active_user_id);
 		}
 		
+		$table->groupBy('another_user')
+		
+		return $
 		
 	}
 }
