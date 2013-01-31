60f0e726cff3a5949f6d4fc6432a36c483d6eb0c
ivorobioff <i_am_vib@yahoo.com>
2013-01-31 08:06:14 +0200

    dsfsd

--- controllers/messages.php
+++ controllers/messages.php
@@ -3,6 +3,13 @@ class Controllers_Messages extends Controllers_Common
 {
 	public function index($parmas = array())
 	{
+		$model = new Models_Dialogs();
+
+		if ($user_id = intval(always_set($parmas, 0, 0)))
+		{
+			$this->_view->assign('active_dialog', $model->getFakeOne($user_id));
+		}
+
 		$this->_view->render('messages/index.phtml');
 	}
 }
---
+++ models/dialogs.php
@@ -0,0 +1,13 @@
+<?php
+class Models_Dialogs
+{
+	public function get4Main()
+	{
+
+	}
+
+	public function getFakeOne($user_id)
+	{
+
+	}
+}
