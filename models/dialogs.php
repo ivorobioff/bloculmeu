118a38a1af8802aa00feb9e35fb85a5ca2474cf7
Igor Vorobioff <i_am_vib@yahoo.com>
2013-01-31 16:51:48 +0200

    Update models/dialogs.php
    dialogs

--- models/dialogs.php
+++ models/dialogs.php
@@ -1,13 +1,15 @@
 <?php
 class Models_Dialogs
 {
-	public function get4Main()
+	public function get4Main($active_user_id = 0)
 	{
-
-	}
-
-	public function getFakeOne($user_id)
-	{
-
+		$table = new Db_Messages();
+		$table
+			->where('(sender_id='.Db_Currents::getUserInfo('id').') OR (receiver_id='.Db_Currents::getUserInfo('id').')');
+		
+		if ($active_user_id)
+		{
+			$table->where('sender_id != '.$active_user_id.' OR receiver_id != '.$active_user_id);
+		}
 	}
 }
