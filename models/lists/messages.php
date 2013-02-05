1a6a49da84bafd2ce9aeeef0a8dd9f71cd61d501
Igor Vorobioff <i_am_vib@yahoo.com>
2013-02-05 09:37:24 +0200

    Update models/lists/messages.php
    ssdfs

--- models/lists/messages.php
+++ models/lists/messages.php
@@ -1,6 +1,15 @@
 <?php
 class Models_Lists_Messages extends Libs_IteratorReplacer
 {
+	private $_profile_table = null;
+	
+	public function __construct($data)
+	{
+		parent::__construct($data);
+		$this->_profile_table = new Db_Profiles();
+		$this->_profile_table->setAlias('p');
+	}
+	
 	public function current()
 	{
 		$res = current($this->_data);
