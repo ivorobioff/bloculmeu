c83dbd81f01779d38bfef9174ebe3b1093bb44cb
Igor Vorobioff <i_am_vib@yahoo.com>
2013-02-05 09:49:46 +0200

    Update models/lists/messages.php
    asdas

--- models/lists/messages.php
+++ models/lists/messages.php
@@ -1,18 +1,30 @@
 <?php
 class Models_Lists_Messages extends Libs_IteratorReplacer
 {
-	private $_profile_table = null;
+	private $_profiles_table = null;
 	
 	public function __construct($data)
 	{
 		parent::__construct($data);
-		$this->_profile_table = new Db_Profiles();
-		$this->_profile_table->setAlias('p');
+		$this->_profiles_table = new Db_Profiles();
 	}
 	
 	public function current()
 	{
 		$res = current($this->_data);
+		
+		if (isset($res['another_user']))
+		{
+			
+		}
+		else
+		{
+			
+		}
+		
+		$res['fio'] = $this->_profiles_table
+			->where('user_id', $res['another_user'])
+		
 		return $res;
 	}
 }
