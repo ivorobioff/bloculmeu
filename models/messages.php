ad97efc10df74cb2ed31087fa8d2f652ee6847a6
Igor Vorobioff <i_am_vib@yahoo.com>
2013-02-05 09:26:09 +0200

    Update models/messages.php
    messages

--- models/messages.php
+++ models/messages.php
@@ -11,15 +11,23 @@ class Models_Messages
 
 	public function get4User($user_id)
 	{
+		if ($user_id == 0)
+		{
+			return array();
+		}
+		
 		$profile_table = new Db_Profiles();
 		$profile_table->setAlias('p');
 
 		$main_where = '(m.sender_id='.$user_id.' AND m.receiver_id='.Db_Currents::getUserInfo('id')
 				.') OR (m.receiver_id='.$user_id.' AND m.sender_id='.Db_Currents::getUserInfo('id').')';
 
-		return $this->_table
-			
+		$data = $this->_table
+			->select('IF(m.sender_id="'.Db_Currents::getUserInfo('id').'", m.receiver_id, m.sender_id) AS another_user')
+			->select('IF(m.sender_id="'.Db_Currents::getUserInfo('id').'", m.sender_id, m.receiver_id) AS me')
 			->where($main_where)
 			->fetchAll();
+			
+		return new Models_List_Messages($data);
 	}
 }
