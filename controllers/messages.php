2ed2295edf2f786e4823751bd324a7e080b087cd
Igor Vorobioff <i_am_vib@yahoo.com>
2013-02-06 09:02:05 +0200

    Update controllers/messages.php
    ssdf

--- controllers/messages.php
+++ controllers/messages.php
@@ -11,10 +11,9 @@ class Controllers_Messages extends Controllers_Common
 		}
 
 		$model = new Models_Dialogs();
-		$messages_model = new Models_Messages();
-
+	
 		$this->_view->assign('dialogs_list', $model->get4Main($active_user_id));
-		$this->_view->assign('messages_list', json_encode($messages_model->get4User($active_user_id)));
+		$this->_view->assign('active_user_id', $active_user_id);
 
 		$this->_view->render('messages/index.phtml');
 	}
